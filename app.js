66e432d38fb673bb7e7affd5cc925babb8d1df58
Lucas Macedo <lucasmacedo1901@gmail.com>
2025-01-23 16:56:52 -0300

    lance 1

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let nomesAmigos = [];
-let incluirAmigo = "";
+
+function exibirTexto (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+
 
 function adicionarAmigo() {
+    let incluirAmigo = document.querySelector('input').value
     nomesAmigos.push(incluirAmigo);
+    console.log(nomesAmigos);
+    limparLabel();
+    exibirTexto('ul', nomesAmigos);
 }
+    
+function limparLabel() {
+    incluirAmigo = document.querySelector('input');
+    incluirAmigo.value = '';
+
+
+}
+
+//Sorteio
+
+function sortearAmigo() {
+    let ganhador = '';
+    let i;
+    let maximo = nomesAmigos.length;
+    i = parseInt(Math.random() * maximo);
+    ganhador = nomesAmigos[i];
+
+    //Escolhe o ganhador
+
+    exibirTexto('section', ganhador) //Exibe o ganhador
+
+}
+
